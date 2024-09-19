094bbec2781dd4bdaf4d275b93c1fab55b0fa4e3
Francesco Sullo <sullof@gmail.com>
2024-09-19 14:08:30 +0200

    Restructuring directories

--- contracts/examples/upgradeable-services/SimpleERC20Wallet.sol
+++ contracts/examples/upgradeable-services/SimpleERC20Wallet.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC7656ServiceExt} from "../../extensions/ERC7656ServiceExt.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {StorageSlot} from "@openzeppelin/contracts/utils/StorageSlot.sol";
-import {IGuardian} from "../guardians/IGuardian.sol";
+import {IGuardian} from "../../utils/guardians/IGuardian.sol";
 
 //import "hardhat/console.sol";
 
